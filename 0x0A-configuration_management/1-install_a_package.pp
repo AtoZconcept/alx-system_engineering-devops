28ccfb7f98992495d5f11fea3781f1548ee893ba
AtoZconcept <opeyemiolaafeez.com>
2024-01-19 23:27:42 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # install flask from pip3
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin', '/usr/local/bin'],
-  refreshonly => true,
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
