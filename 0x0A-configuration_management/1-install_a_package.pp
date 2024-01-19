fee65068430bc0044e7cc69588b15108e18cccd5
AtoZconcept <opeyemiolaafeez.com>
2024-01-20 00:09:19 +0100

    task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup
 # install flask from pip3
-
-package { 'Flask':
+exec { 'Flask':
+  command  => 'apt install flask',
   ensure   => '2.1.0',
   provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # create a manifest that kills a process named killmenow
 exec { 'killmenow_process':
-  command => 'pkill -f "killmenow"',
-  onlyif  => 'pgrep -f "killmenow"',
+  command   => 'pkill killmenow',
+  provider  => 'shell',
 }
