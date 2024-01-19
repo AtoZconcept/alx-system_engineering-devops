010ee0b2479b79c53aafc9291403080139942756
AtoZconcept <opeyemiolaafeez.com>
2024-01-19 23:11:52 +0100

    add

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,1 +1,6 @@
 # install flask from pip3
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  refreshonly => true,
+}
