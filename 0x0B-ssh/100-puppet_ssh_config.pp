6d03c775df8fcdc831f49b8b915a6208702a4cdc
AtoZconcept <opeyemiolaafeez.com>
2024-01-22 23:27:46 +0100

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,10 @@
 # puppet file to to change ssh/config file
-file { '/root/.ssh/config':
-  ensure  => present,
-  mode    => '0600',
-  content => '',
-}
-
 file_line { 'Turn off passwd auth':
-  path  => '/root/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication.*$',
 }
 
 file_line { 'Declare identity file':
-  path  => '/root/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^IdentityFile.*$',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school',
 }
