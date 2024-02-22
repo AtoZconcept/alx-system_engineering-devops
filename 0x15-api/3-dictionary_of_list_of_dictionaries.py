2f200c71a86050c230e14c40e5fddf8b0ab4bd8b
AtoZconcept <opeyemiolaafeez.com>
2024-02-22 13:53:23 +0100

    add

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,8 +4,23 @@
 
 import json
 import requests
-import sys
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('Usage: ', sys.argv[0], '<employee id>')
-        sys.exit(1)
+    url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get('{}/users/'.format(url)).json()
+
+    dict_data = {}
+    for user in users:
+        user_id = user.get("id")
+        todos = requests.get(url + '/users/{}/todos'.format(user_id)).json()
+
+        task_list = []
+        for task in todos:
+            task_list.append(
+                {"username": user.get('username'),
+                    "task": task.get('title'),
+                    "completed": task.get('completed')}
+            )
+        dict_data[user_id] = task_list
+
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(dict_data, file)
