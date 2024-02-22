d1f0ab3b35210bb6826fffef603ee8b43825292d
AtoZconcept <opeyemiolaafeez.com>
2024-02-22 13:12:50 +0100

    add

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""script to export data in the JSON format"""
+
+
+import json
+import sys
+import requests
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print('Usage: ', sys.argv[0], '<employee id>')
+        sys.exit(1)
+
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(url + '/users/{}'.format(user_id)).json()
+    todos = requests.get(url + '/users/{}/todos'.format(user_id)).json()
+
+    task_list = []
+    for task in todos:
+        if task.get('userId') == int(user_id):
+            task_list.append(
+                {"task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.get('username')}
+            )
+        json_data = {user_id: task_list}
+    with open('{}.json'.format(user_id), 'w') as file:
+        json.dump(json_data, file)
