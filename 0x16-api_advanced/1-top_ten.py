c1869331ec8a37b81a5f4618d0573ec45bd34989
AtoZconcept <opeyemiolaafeez.com>
2024-03-05 20:05:34 +0100

    add

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,12 @@ of the first 10 hot posts listed for a given subreddit
 
 def top_ten(subreddit):
     """function to return first top ten posts"""
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
     header = {'User-Agent': 'Mozilla/5.0 (Linux x86_64) Edge109.0'}
     response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
+        data = response.json()
+        for post in data['data']['children']:
+            print(post['data']['title'])
     else:
         return 0
